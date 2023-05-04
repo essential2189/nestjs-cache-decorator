ee475e119e09f4586c70d536e983e602c81adc3c
왕승재 <essential2189@gmail.com>
2023-05-04 14:01:46 +0900

    fix : controller cache decorator

--- src/cache.decorator.ts
+++ src/cache.decorator.ts
@@ -1,6 +1,7 @@
-import { applyDecorators, SetMetadata } from "@nestjs/common";
+import { applyDecorators, SetMetadata, UseInterceptors } from "@nestjs/common";
 import { CACHE_OPTIONS, CACHE_STORE } from "./constant";
 import { CacheOptions } from "./interfaces";
+import { ControllerCacheInterceptor } from "./controller-cache.interceptor";
 
 /**
  * Defining Decorator Functions by Local Cache
@@ -18,4 +19,16 @@ export function LocalCache(options: CacheOptions = {}): MethodDecorator {
  */
 export function RedisCache(options: CacheOptions = {}): MethodDecorator {
   return applyDecorators(SetMetadata(CACHE_OPTIONS, options), SetMetadata(CACHE_STORE, "redis"));
+}
+
+/**
+ * Defining Decorator Functions by Redis Cache
+ * @param options
+ * @constructor
+ */
+export function APICache(options: CacheOptions = {}): MethodDecorator {
+  return applyDecorators(
+    UseInterceptors(ControllerCacheInterceptor),
+    SetMetadata(CACHE_OPTIONS, options),
+  );
 }
